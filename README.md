6fb1f81bb054ea7fd7efc6d5758691619a2009f8
jianping liu <rabbit.ljp@gmail.com>
2012-05-03 14:03:50 +0800

    add folder

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 boost-coro
 ==========
+
+libcoro
+
+libev
---
+++ build/libcoro/CMakeFiles/coro.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/Users/liujianping/github/boost-coro/libcoro/coro.c" "/Users/liujianping/github/boost-coro/build/libcoro/CMakeFiles/coro.dir/coro.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "CORO_ASM=1"
+  "CORO_ASM=1"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
